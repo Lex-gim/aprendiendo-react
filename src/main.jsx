d25d143aa9a9f201da65784d9c98a6ad48f57146
Miguel Ángel <vasquezmazo.miguelangel.112@gmail.com>
2024-10-10 14:47:39 -0500

    Primer cambio

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,11 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const root = createRoot(document.getElementById('root'))
-root.render("holabola")
+root.render(
+    <>
+        <button>Ayerrr</button>
+        <button>Hoyyyy</button>
+        <button>Mañana</button>
+    </>
+
+)
